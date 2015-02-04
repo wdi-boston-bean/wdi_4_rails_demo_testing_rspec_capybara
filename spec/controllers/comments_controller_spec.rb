0bd8d2ca146ebab80914b0c8b5ac7ad213d26532
lady3bean <annehawleybrett@gmail.com>
2015-02-04 17:25:13 -0500

    returns OK http response

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,1 +1,23 @@
+require 'rails_helper'
 
+RSpec.describe CommentsController do
+  let(:valid_attributes) {
+    {body: 'so so so'}
+  }
+  let(:invalid_attributes) {
+    {body: nil }
+  }
+
+  describe "get index" do
+    it 'has a 200 status code' do
+      @article = Article.create!(title: "boo", body: "moo")
+      @article.comments.create!(body: "cultural misunderstanding ensues")
+      expect(response.status).to eq 200
+    end
+    
+    it 'renders the index template' do
+      expect(response).to render_template('index')
+    end
+  end
+
+end
