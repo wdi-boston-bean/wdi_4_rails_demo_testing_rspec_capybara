ac295c0aba80034ae47cacefcdd67a34dc22f6d6
lady3bean <annehawleybrett@gmail.com>
2015-02-04 15:51:06 -0500

    succesfully indexes comments, returns routing error you'd expect

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,1 +1,19 @@
+class CommentsController < ApplicationController
 
+  def index
+    @comments = Comment.all
+  end
+
+  def new
+    @article = Article.find(params[:id])
+    @comment = @article.comments(comment_params)
+    
+  end
+
+  private
+    def comment_params
+      params.require(:comments).permit(:body)
+    end
+
+
+end
--- spec/features/comments_spec.rb
+++ spec/features/comments_spec.rb
@@ -1,1 +1,16 @@
+require 'rails_helper'
 
+RSpec.feature 'Manages Comments' do
+  scenario 'lists first three comments' do
+    @article = Article.create!(title: "Sweet", body: "Beat")
+    @article.comments.create!(body: "blah")
+    @article.comments.create!(body: "blue")
+    @article.comments.create!(body: "blood")
+
+    visit 'article/1/comments'
+
+    expect(page).to have_content 'Comments'
+    expect(page).to have_selector 'comment', count: 3
+  end
+
+end
