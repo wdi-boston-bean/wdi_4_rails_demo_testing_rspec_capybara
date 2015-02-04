bc64ad3222fd6208992efeda6cb0d57dcd14ee0e
lady3bean <annehawleybrett@gmail.com>
2015-02-04 16:54:51 -0500

    passes index routing test

--- spec/routing/comments_routing_spec.rb
+++ spec/routing/comments_routing_spec.rb
@@ -1,1 +1,9 @@
+require 'rails_helper'
 
+RSpec.describe 'routes articles/1/comments to route to comment controller' do
+   it 'routes GET /articles/1/commments to the comments controller' do
+    expect(get('/articles/1/comments')).to route_to('comments#index', article_id: '1')
+  end
+end
+
+#can hard code ids in spec bc it is testing the parsing and routing of the string passed
