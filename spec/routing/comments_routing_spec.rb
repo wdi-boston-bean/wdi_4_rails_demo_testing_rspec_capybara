31cf2ea5cac5495350fcf41d49c1395f3a2ea677
lady3bean <annehawleybrett@gmail.com>
2015-02-04 17:09:27 -0500

    comments routing tests complete and passing

--- spec/routing/comments_routing_spec.rb
+++ spec/routing/comments_routing_spec.rb
@@ -4,6 +4,16 @@ RSpec.describe 'routes articles/1/comments to route to comment controller' do
    it 'routes GET /articles/1/commments to the comments controller' do
     expect(get('/articles/1/comments')).to route_to('comments#index', article_id: '1')
   end
+
+   it 'routes GET /articles/1/comments/new to the comments controller' do
+    expect(get('/articles/1/comments/new')).to route_to('comments#new', article_id: '1')
+  end
+
+  it 'routes POST /articles/1/comments to the comments controller' do
+    expect(post('/articles/1/comments')).to route_to('comments#create', article_id: '1')
+  end
+
 end
 
 #can hard code ids in spec bc it is testing the parsing and routing of the string passed
+
